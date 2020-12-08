d687cfb349efe85822ce4f723b85fde68a94d275
Александр Долганов <dalex3734@yandex.ru>
2020-12-08 14:36:55 +0300

    Updates

---
+++ precache-manifest.34c72923fa755991e93be2dd0c7de9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b620239be7c98e75a27a4e403329220a",
+    "url": "/infomaximum/index.html"
+  },
+  {
+    "revision": "2607eed814c4680be2b4",
+    "url": "/infomaximum/static/css/main.bb8189b1.chunk.css"
+  },
+  {
+    "revision": "650da1df5c08296dc28f",
+    "url": "/infomaximum/static/js/2.46bc2745.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infomaximum/static/js/2.46bc2745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2607eed814c4680be2b4",
+    "url": "/infomaximum/static/js/main.0a351186.chunk.js"
+  },
+  {
+    "revision": "dc584f18d60c4cac56d1",
+    "url": "/infomaximum/static/js/runtime-main.67056704.js"
+  }
+]);
